8d2a8ff094d6f3921e2b35960e580f21ab04e27a
~Chocolxt~ <dhsepulvedas@udistrital.edu.co>
2024-04-26 16:49:39 -0500

    feat: :zap:  Reformulating code in (screenMain)

--- app/src/main/java/com/udppcmyplantsitter/view/screenMain.kt
+++ app/src/main/java/com/udppcmyplantsitter/view/screenMain.kt
@@ -10,21 +10,15 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -32,32 +26,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-
 import com.example.udppcmyplantsitter.ui.theme.MainColor
 import com.example.udppcmyplantsitter.viewModel.appNavegation.appScreens
-
-
-
-///////////
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.SearchBar
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.toSize
 import com.example.udppcmyplantsitter.R
 import com.example.udppcmyplantsitter.ui.theme.SecondColor
 import com.udppcmyplantsitter.dataManagement.Helper
@@ -68,9 +49,7 @@ import com.udppcmyplantsitter.dataManagement.AssignmentService
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun screenMain(navController: NavController){
-    //NavigationDrawer()
     val context = LocalContext.current
-    //link repo
     var query by remember { mutableStateOf("") }
     var active by remember { mutableStateOf(false) }
     val onSearch: (String) -> Unit = {
