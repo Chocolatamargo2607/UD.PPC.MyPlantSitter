d03649ca9bd92df7fb4e4ba9a4df3b4f40ad9b07
~Chocolxt~ <dhsepulvedas@udistrital.edu.co>
2024-06-03 17:18:23 -0500

    fet: :hammer: Modifying (screenLogin)

--- app/src/main/java/com/udppcmyplantsitter/view/screenLogin.kt
+++ app/src/main/java/com/udppcmyplantsitter/view/screenLogin.kt
@@ -1,33 +1,50 @@
 package com.udppcmyplantsitter.view
 
+import android.net.wifi.hotspot2.pps.Credential.UserCredential
+import android.util.Log
+import android.widget.Toast
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.PointerId
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -39,82 +56,214 @@ import com.example.udppcmyplantsitter.viewModel.appNavegation.appScreens
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun screenLogin(navController: NavController){
-
-
-
-    var nameUser by remember {
-        mutableStateOf("")
+// True = Login; False= Create
+    val context = LocalContext.current
+    val showLoginForm = rememberSaveable{
+        mutableStateOf(true)
     }
-    var password by remember {
-        mutableStateOf("")
-    }
-    Row() {
-        TopAppBar(
-            title = { Text(text=" My Plant Sitter ღ") },
-            colors = TopAppBarDefaults.topAppBarColors(
-                MainColor,
-                titleContentColor = Color.White
-            ),
-            navigationIcon = {
-                IconButton(onClick ={ navController.navigate(route = appScreens.screenWelcome.router)}) {
-                    Icon(
-                        imageVector = Icons.Default.ArrowBack,
-                        contentDescription = null,
-                        tint = Color.White
-                    )
+    Surface(modifier = Modifier.fillMaxSize()
+    ) {
+        Row() {
+            TopAppBar(
+                title = { Text(text=" My Plant Sitter ღ") },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    MainColor,
+                    titleContentColor = Color.White
+                ),
+                navigationIcon = {
+                    IconButton(onClick ={ navController.navigate(route = appScreens.screenWelcome.router)}) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = null,
+                            tint = Color.White
+                        )
 
+                    }
+                }
+            )
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ){
+            if( showLoginForm.value){
+                Text(text = "Login with your account")
+                UserForm(
+                    isCreateAccount = false
+                ){
+                        email, password ->
+                    Log.d("PlantsTrue", "Logged in with $email & $password")
+
+                    Toast.makeText(
+                        context, "Log in True", Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }else{
+                Text(text = "Register an account")
+                UserForm(
+                    isCreateAccount = true
+                ){
+                        email, password ->
+                    Log.d("PlantsTrue", "Registered with $email & $password")
+
+                    Toast.makeText(
+                        context, "Register True", Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
-        )
-    }
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ){
-
-        TextField(
-            value = nameUser,
-            onValueChange = {
-                nameUser = it
-            },
-            label = {
-                Text(text = "User Name", color = Color.White)
-            },
-            colors = TextFieldDefaults.textFieldColors(
-                containerColor = SecondColor,
-                unfocusedIndicatorColor = MainColor
-            )
-
-
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(text = "Insert your password")
-        Spacer(modifier = Modifier.height(16.dp))
-        TextField(
-            value = password,
-            onValueChange = {
-                password = it
-            },
-            placeholder = {
-                Text(text = "Password", color = Color.White)
-            },
-            colors = TextFieldDefaults.textFieldColors(
-                containerColor = SecondColor,
-                unfocusedIndicatorColor = MainColor
-
-            )
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { navController.navigate(route = appScreens.tabsMovements.router)},
-            colors = ButtonDefaults.buttonColors(MainColor)) {
-            Text(text = "Log in")
+            Spacer(modifier = Modifier.height(15.dp))
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                val question=
+                    if (showLoginForm.value)"Don't have an account?"
+                    else "Already have an account?"
+                val option=
+                    if(showLoginForm.value)"Register"
+                    else "Log in"
+                Text(text = question)
+                Text(text = option,
+                    modifier = Modifier
+                        .clickable { showLoginForm.value = !showLoginForm.value }
+                        .padding(start = 5.dp),
+                    color= SecondColor
+                )
+            }
         }
-
     }
 
+
+}
+
+@Composable
+fun UserForm(
+    isCreateAccount: Boolean = false,
+    onDone: (String,String) -> Unit={email,pwd ->}
+){
+    val email= rememberSaveable {
+        mutableStateOf("")
+    }
+    val password = rememberSaveable {
+        mutableStateOf("")
+    }
+
+    val passwordVisible= rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    val valido = remember(email.value,password.value){
+        email.value.trim().isNotEmpty() &&
+                password.value.trim().isNotEmpty()
+    }
+
+    val keyboardController= LocalSoftwareKeyboardController.current
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        EmailImput(
+            emailState= email
+        )
+        PasswordInput(
+            passwordState= password,
+            labelId= "Password",
+            passwordVisible= passwordVisible
+        )
+        SubmitButton(
+            textId= if( isCreateAccount)"Register" else "Log in",
+            inputValido= valido
+        ){
+            onDone(email.value.trim(), password.value.trim())
+            keyboardController?.hide()
+        }
+    }
+}
+
+@Composable
+fun SubmitButton(textId: String,inputValido:Boolean, onClic: ()-> Unit) {
+    Button(onClick = onClic,
+        enabled = inputValido,
+        colors = ButtonDefaults.buttonColors(MainColor)){
+        Text(text = textId,
+            modifier= Modifier
+                .padding(5.dp))
+    }
+}
+
+@Composable
+fun PasswordInput(
+    passwordState: MutableState<String>,
+    labelId: String,
+    passwordVisible: MutableState<Boolean>
+){
+    val visualTransformation=
+        if (passwordVisible.value)
+            VisualTransformation.None
+        else PasswordVisualTransformation()
+
+
+    OutlinedTextField(
+        value = passwordState.value,
+        onValueChange ={passwordState.value= it},
+        label= { Text(text = labelId)},
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Password
+        ),
+        modifier = Modifier
+            .padding(bottom = 10.dp, start = 10.dp, end = 10.dp)
+            .fillMaxWidth(),
+        visualTransformation = visualTransformation,
+        trailingIcon = {
+            if (passwordState.value.isNotBlank()){
+                PasswordVisibleIcon(passwordVisible)
+            }else null
+        }
+    )
+}
+
+@Composable
+fun PasswordVisibleIcon(passwordVisible: MutableState<Boolean>) {
+    val image= if(passwordVisible.value)
+        Icons.Default.VisibilityOff
+    else
+        Icons.Default.Visibility
+    IconButton(onClick = {
+        passwordVisible.value = !passwordVisible.value
+    }) {
+        Icon(imageVector = image, contentDescription ="")
+    }
+}
+
+@Composable
+fun EmailImput(
+    emailState: MutableState<String>,
+    labelId: String= "Email"
+) {
+    InputField(
+        valueState= emailState,
+        labelId= labelId,
+        keyboardType= KeyboardType.Email
+    )
+}
+
+@Composable
+fun InputField(
+    valueState: MutableState<String>,
+    labelId: String,
+    isSingleLine: Boolean = true,
+    keyboardType: KeyboardType
+){
+    OutlinedTextField(
+        value = valueState.value,
+        onValueChange = {valueState.value = it},
+        label = { Text(text = labelId)},
+        singleLine = isSingleLine,
+        modifier = Modifier
+            .padding(bottom = 10.dp, start = 10.dp, end = 10.dp)
+            .fillMaxWidth(),
+        keyboardOptions = KeyboardOptions(
+            keyboardType= keyboardType
+        )
+    )
 }
 
 @Preview(showBackground = true)
