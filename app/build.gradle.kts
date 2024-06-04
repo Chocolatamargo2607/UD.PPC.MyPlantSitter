e136274738b80e9535dc6823207752b7284d575a
~Chocolxt~ <dhsepulvedas@udistrital.edu.co>
2024-06-04 00:44:47 -0500

    feat: :twisted_rightwards_arrows: Message system

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
     implementation("com.google.firebase:firebase-firestore-ktx")
     implementation ("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-messaging-ktx")
     //
 
     testImplementation(libs.junit)
